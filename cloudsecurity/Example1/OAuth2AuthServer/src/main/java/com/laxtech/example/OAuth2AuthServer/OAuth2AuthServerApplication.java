c0422c41bb902a5706f6abbe23089caa6abd3a1d
sanjeev <gsanjeevtripathi@gmail.com>
2017-09-30 12:35:23 +0530

    moved to example directories

---
+++ cloudsecurity/Example1/OAuth2AuthServer/src/main/java/com/laxtech/example/OAuth2AuthServer/OAuth2AuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.laxtech.example.OAuth2AuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AuthServerApplication.class, args);
+	}
+}
---
+++ cloudsecurity/Example2/my-website/src/main/java/com/laxtech/example/mywebsite/config/WebSecurityConfigurer.java
@@ -0,0 +1,32 @@
+package com.laxtech.example.mywebsite.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+
+/**
+ * The Web Security configuration for My Website.
+ */
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()   // Allow navigating to index page,
+                .anyRequest().authenticated();  // but secure all the other URLs
+    }
+
+    @Bean
+    public OAuth2RestOperations restOperations(
+            OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails, OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(oAuth2ProtectedResourceDetails, oauth2ClientContext);
+    }
+}
