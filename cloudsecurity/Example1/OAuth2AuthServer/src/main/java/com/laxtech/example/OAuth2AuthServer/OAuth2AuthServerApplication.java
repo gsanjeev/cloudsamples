f45a15fcc769efd2b5fc34612642f5f413dbc756
sanjeev <gsanjeevtripathi@gmail.com>
2017-09-30 15:20:26 +0530

    added explanation path

--- cloudsecurity/Example1/OAuth2AuthServer/src/main/java/com/laxtech/example/OAuth2AuthServer/OAuth2AuthServerApplication.java
+++ cloudsecurity/Example1/OAuth2AuthServer/src/main/java/com/laxtech/example/OAuth2AuthServer/OAuth2AuthServerApplication.java
@@ -3,6 +3,7 @@ package com.laxtech.example.OAuth2AuthServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Explanation at http://stytex.de/blog/2016/02/01/spring-cloud-security-with-oauth2/
 @SpringBootApplication
 public class OAuth2AuthServerApplication {
 
