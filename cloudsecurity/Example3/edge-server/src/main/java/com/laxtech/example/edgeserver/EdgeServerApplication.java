545f8aaa01cab441dce8bc893f97bb0172ea79ee
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-06 20:58:56 +0530

    product service code

--- cloudsecurity/Example3/edge-server/src/main/java/com/laxtech/example/edgeserver/EdgeServerApplication.java
+++ cloudsecurity/Example3/edge-server/src/main/java/com/laxtech/example/edgeserver/EdgeServerApplication.java
@@ -2,11 +2,19 @@ package com.laxtech.example.edgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
+@EnableZuulProxy
 public class EdgeServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EdgeServerApplication.class, args);
+		//SpringApplication.run(EdgeServerApplication.class, args);
+
+		new SpringApplicationBuilder(EdgeServerApplication.class).web(true).run(args);
 	}
 }
+
