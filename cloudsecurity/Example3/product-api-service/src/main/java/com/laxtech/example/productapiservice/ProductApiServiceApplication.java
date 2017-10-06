4f9cbb8f56670f048a5714e916f18910193883dd
sanjeev <gsanjeevtripathi@gmail.com>
2017-10-06 13:11:59 +0530

    sucurity

---
+++ cloudsecurity/Example3/product-api-service/src/main/java/com/laxtech/example/productapiservice/ProductApiServiceApplication.java
@@ -0,0 +1,12 @@
+package com.laxtech.example.productapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiServiceApplication.class, args);
+	}
+}
