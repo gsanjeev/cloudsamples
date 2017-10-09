182872bfccefe07459231239f5c85fcbfe49122e
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-10 00:51:18 +0530

    added explaination

--- cloudsecurity/Example3/auth-server/src/main/java/com/laxtech/example/AuthServerApplication.java
+++ cloudsecurity/Example3/auth-server/src/main/java/com/laxtech/example/AuthServerApplication.java
@@ -23,8 +23,25 @@ import java.security.Principal;
 // resource server authenticated apis if only Zuul gateway is accessible to client. With the help of property (sensitiveHeaders) Zuul passes authentication header
 // as is to resource server apis. Spring MVC inject authentication header and principal parameter in resource api method call
 // step1: get authorization code(optional) from auth-server.
+// use url http://localhost:9999/uaa/oauth/authorize?response_type=code&client_id=acme&redirect_uri=http://example.com&scope=webshop& state=97536
 // step2: get token from auth-server.
-// step3: access api using token from product-api-service (resource server).
+//  i) use case: grant type = authorization code. Send post request with basic authentication (user = acme and password= acmesecret) , content type application/x-www-form-urlencoded and set following field in body
+//      grant_type=authorization_code
+//      client_id=acme
+//      redirect_uri=http://example.com
+//      code=code received in step 1.
+//  ii) use case: grant type = implicit use url http://localhost:9999/uaa/oauth/authorize?response_type=token&client_id=acme&redirect_uri=http://example.com&scope=webshop&state=48532
+//  iii) use case: grant type = password. Send post request with basic authentication (user = acme and password= acmesecret) , content type application/x-www-form-urlencoded and set following field in body
+//      grant_type=password
+//      client_id=acme
+//      scope=webshop
+//      username=user
+//      password=password
+//  iv) use case: grant type = client_credentials. Send post request with basic authentication (user = acme and password= acmesecret), content type application/x-www-form-urlencoded and set following field in body
+//      grant_type=client_credentials
+//      scope=webshop
+// step3: access api using token from product-api-service (resource server) through zuul gate. Send get request to http://localhost:8765/api/product/123
+// and set Authorization header to "Bearer token received in above step"  under Headers tab in postman(ex: Bearer 3d34258a-2aa3-481c-86a3-60a362447941).
 // internal step: resource server contact auth-server for token verification and get the user detail using userInfoUri
 //reference http://callistaenterprise.se/blogg/teknik/2015/04/27/building-microservices-part-3-secure-APIs-with-OAuth/
 //question? why there is separate layer for resource server why can't annotate product-service as resource service.
