701e4408a8a93bcafd4940e959500b082733b435
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-11 01:05:56 +0530

    security example 2

--- cloudsecurity/Example2/my-auth-server/src/main/java/com/laxtech/example/cloudsecurity/MyAuthServerApplication.java
+++ cloudsecurity/Example2/my-auth-server/src/main/java/com/laxtech/example/cloudsecurity/MyAuthServerApplication.java
@@ -11,6 +11,9 @@ Three main part of example
     i) client - my-website client application which uses spring mvc and spring security ii) resource server - /auth/user resource. ResourceServerConfiguration uses @EnableResourceServer annotation
     iii) authentication server - AuthorizationServerConfigurer uses @EnableAuthorizationServer annotation. This uses Spring MVC to display user pages. iv) users are are user and admin.
     Spring MVC is secured with Spring Security
+Remember OAuth2 does not provide authentication. We are using Spring Security for this. OAuth2 is delegation protocol. Third party does not share credentials with client(my-website).
+They share credentials with Authentication Provider (like spring security with memory/rdbm/ldap). OAuth2 facilitate this.
+Login page and user consensus page resides in auth-server application not in client application.
 reference: https://blog.jdriven.com/2016/09/securing-application-landscape-spring-cloud-security-part-1/
 and https://bitbucket.org/rlippolis/cloud-security-example.git tag secure-website
  */
