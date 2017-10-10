99f999fad0ed62850cfe1a12987fb402efad6251
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-10 23:52:31 +0530

    security example 2

--- cloudsecurity/Example2/my-auth-server/src/main/java/com/laxtech/example/cloudsecurity/MyAuthServerApplication.java
+++ cloudsecurity/Example2/my-auth-server/src/main/java/com/laxtech/example/cloudsecurity/MyAuthServerApplication.java
@@ -9,7 +9,7 @@ Authentication code, and access token requests from my-website are taken care by
     ii) Authorization Server and User Info properties in application.yml. These properties configure my-website to use the URLs provided by my-auth-server
 Three main part of example
     i) client - my-website client application which uses spring mvc and spring security ii) resource server - /auth/user resource. ResourceServerConfiguration uses @EnableResourceServer annotation
-    iii) authentication server - AuthorizationServerConfigurer uses @EnableAuthorizationServer annotation. This uses Spring MVC to display user pages.
+    iii) authentication server - AuthorizationServerConfigurer uses @EnableAuthorizationServer annotation. This uses Spring MVC to display user pages. iv) users are are user and admin.
     Spring MVC is secured with Spring Security
 reference: https://blog.jdriven.com/2016/09/securing-application-landscape-spring-cloud-security-part-1/
 and https://bitbucket.org/rlippolis/cloud-security-example.git tag secure-website
