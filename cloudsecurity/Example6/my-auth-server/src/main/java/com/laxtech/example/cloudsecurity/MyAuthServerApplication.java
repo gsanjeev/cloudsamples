ab27b80e61c7dbc3fd879025c40da1b00e72ab61
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-12 00:22:07 +0530

    ininital commit security/example6

---
+++ cloudsecurity/Example6/my-auth-server/src/main/java/com/laxtech/example/cloudsecurity/MyAuthServerApplication.java
@@ -0,0 +1,41 @@
+package com.laxtech.example.cloudsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+*******************************Example 2************************************************************************************************************
+This example uses authorization_code grant type and Spring MVC client(my-website) not the test clients like browser for authentication code and postman.
+Authentication code, and access token requests from my-website are taken care by following
+    i)WebSecurityConfigurerAdapter implementation is annotated with @EnableOAuth2Sso
+    ii) Authorization Server and User Info properties in application.yml. These properties configure my-website to use the URLs provided by my-auth-server
+Three main part of example
+    i) client - my-website client application which uses spring mvc and spring security ii) resource server - /auth/user resource. ResourceServerConfiguration uses @EnableResourceServer annotation
+    iii) authentication server - AuthorizationServerConfigurer uses @EnableAuthorizationServer annotation. This uses Spring MVC to display user pages. iv) users are are user and admin.
+    Spring MVC is secured with Spring Security
+Remember OAuth2 does not provide authentication. We are using Spring Security for this. OAuth2 is delegation protocol. Third party does not share credentials with client(my-website).
+They share credentials with Authentication Provider (like spring security with memory/rdbm/ldap). OAuth2 facilitate this.
+Login page and user consensus page resides in auth-server application not in client application.
+reference: https://blog.jdriven.com/2016/09/securing-application-landscape-spring-cloud-security-part-1/
+and https://bitbucket.org/rlippolis/cloud-security-example.git tag secure-website
+*******************************Example 4 ************************************************************************************************************
+* **tag -- secure-website-and-rest-api -- Make sure My Website passes the OAuth2 access token to My REST API when fetching the time******************************
+New application, my-rest-api is resource server annotated with @EnableResourceServer which is using Spring Security(@EnableWebSecurity) and OAuth2 protocol(application.yml)
+OAuth2RestTemplate in my-website application is passing user and access token from OAuth2ClientContext to my-rest-api. This is Single Sign On(SSO) Configuration. In example 3 we didn't pass
+access token to other application. In fact we manually passed access token from postman to resource server api.
+*******************************Example 5 ************************************************************************************************************
+**tag -- proxy-api-calls--Only pass --the OAuth2 access token to our internal API (My REST API). Make sure the token is not leaked to some external API******************************
+my-website application is also working as gateway/ edgeserver (@EnableZuulProxy)
+route internalApi: Proxies the calls to http://localhost:8080/api/* to my-rest-api at http://localhost:8081/* and automatically includes our OAuth2 token in the request headers
+route externalApi: do not pass any authentication to the external API (application.yml)
+my-rest-api resource configured as @EnableGlobalMethodSecurity(prePostEnabled = true) and currentTime() api is authorized based on role.
+Can edge server (zuul) be deployed as separate sever like example 3? Finally I separated zuul gateway server out from my-website client application.
+*******************************Example 6 (this) ************************************************************************************************************
+**tag -- stateless-security-with-jwt--Use JSON Web Tokens (JWT) in our REST API, to enable stateless security, instead of checking My Auth Server for every request received from My Website.******************************
+ */
+@SpringBootApplication
+public class MyAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAuthServerApplication.class, args);
+	}
+}
---
+++ cloudsecurity/Example6/my-website/src/main/java/com/laxtech/example/mywebsite/controller/MyWebsiteController.java
@@ -0,0 +1,68 @@
+package com.laxtech.example.mywebsite.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Web MVC Controller serving two pages:
+ *
+ * - http://localhost:8080/ -> Shows 'Hello world!'
+ * - http://localhost:8080/time -> Shows a page with the current time
+ */
+@Controller
+public class MyWebsiteController {
+    private static final Logger LOG = LoggerFactory.getLogger(MyWebsiteController.class);
+
+    @Value("${time.url}")
+    private String timeUrl;
+
+    // Inject the RestOperations. This will be an instance of org.springframework.security.oauth2.client.OAuth2RestTemplate
+    // which is made available as a Bean by our WebSecurityConfigurer.
+    @Autowired
+    private RestOperations restOperations;
+
+    /**
+     * Default index page to verify that our application works.
+     */
+    @RequestMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello world!";
+    }
+
+    /**
+     * Return a ModelAndView which will cause the 'src/main/resources/templates/time.html' template to be rendered,
+     * with the current time.
+     */
+    @RequestMapping("/time")
+    public ModelAndView time() {
+        ModelAndView mav = new ModelAndView("time");
+        mav.addObject("currentTime", getCurrentTime());
+        return mav;
+    }
+
+    /**
+     * Get the time from an external REST API using the injected RestOperations.
+     */
+    private String getCurrentTime() {
+        try {
+            LOG.info("getCurrentTime(): Auth={} ", ((OAuth2RestOperations)restOperations).getAccessToken());//log for testing only.
+            String time = restOperations.getForObject(timeUrl, String.class);
+            LOG.info("getCurrentTime(): Time= {}", time);
+
+            return time;
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
+
+
