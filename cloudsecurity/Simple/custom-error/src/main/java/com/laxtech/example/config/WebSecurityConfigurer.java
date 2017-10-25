244b568c41a20976cc3eb21cbef49491d799cfaa
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-26 01:13:48 +0530

    Security Simple

---
+++ cloudsecurity/Simple/custom-error/src/main/java/com/laxtech/example/config/WebSecurityConfigurer.java
@@ -0,0 +1,62 @@
+package com.laxtech.example.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The Web Security configuration for My Website.
+ */
+@Configuration
+@EnableOAuth2Sso
+@Order(3)
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+//                .authenticated();
+        // @formatter:off
+        http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+                .authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        // @formatter:on
+    }
+
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+        return new OAuth2RestTemplate(resource, context);
+    }
+//
+//    @Bean
+//    public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+//        return map -> {
+//            String url = (String) map.get("organizations_url");
+//            @SuppressWarnings("unchecked")
+//            List<Map<String, Object>> orgs = template.getForObject(url, List.class);
+//            if (orgs.stream()
+//                    .anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+//                return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+//            }
+//            throw new BadCredentialsException("Not in Spring Projects origanization");
+//        };
+//    }
+
+}
+
