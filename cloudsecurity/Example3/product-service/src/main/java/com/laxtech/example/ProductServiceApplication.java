06f680df00ee505996d995a72b99d45ca376fa17
Sanjeev <gsanjeevtripathi@gmail.com>
2018-05-07 00:11:52 +0530

    Token Relay to downstream services

--- cloudsecurity/Example3/product-service/src/main/java/com/laxtech/example/ProductServiceApplication.java
+++ cloudsecurity/Example3/product-service/src/main/java/com/laxtech/example/ProductServiceApplication.java
@@ -2,10 +2,8 @@ package com.laxtech.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableResourceServer
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
